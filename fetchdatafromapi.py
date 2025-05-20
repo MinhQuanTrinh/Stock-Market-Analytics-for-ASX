30af8786e2855547613d6505a77ce282eb39863f
Harry <trinhminhquan2810@gmail.com>
2025-05-21 00:26:47 +0800

    Add Fetching Data Script

---
+++ fetchdatafromapi.py
@@ -0,0 +1,16 @@
+import yfinance as yf
+import boto3
+import pandas as pd
+import json
+from datetime import date
+
+tickers = ['TMK.AX', 'DTR.AX', 'NDQ.AX', 'CBA.AX', 'RMD.AX']
+df = yf.download(tickers, period='1d', interval='1h', group_by='ticker')
+
+json_data = data.to_json()
+
+# Configure AWS S3 Bucket for source data
+s3 = boto3.client('s3')
+s3.put_object(Bucket='asx-stock-raw-data-bucket', 
+              Key=f"raw/{date.today().isoformat()}/stock.json",
+              Body=json_data)
